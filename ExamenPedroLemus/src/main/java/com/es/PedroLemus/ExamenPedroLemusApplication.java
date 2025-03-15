0370647edbc16528511baed3cee19d4249044080
Pedro Lemus <pedrolemuz@gmail.com>
2025-03-15 10:28:09 +0100

    Examen Final

---
+++ ExamenPedroLemus/src/main/java/com/es/PedroLemus/ExamenPedroLemusApplication.java
@@ -0,0 +1,13 @@
+package com.es.PedroLemus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPedroLemusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPedroLemusApplication.class, args);
+	}
+
+}
---
+++ ExamenPedroLemus/src/test/java/com/es/PedroLemus/ExamenPedroLemusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.PedroLemus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPedroLemusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
